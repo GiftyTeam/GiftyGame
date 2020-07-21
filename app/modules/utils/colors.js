76d01843aee2b6808516e3e8ca9914de0e3bee3f
JavidHaji-zada <JavidHaji-zada@users.noreply.github.com>
2020-07-21 16:58:43 +0300

    photo screen ready with timer

--- app/modules/utils/colors.js
+++ app/modules/utils/colors.js
@@ -1,9 +1,10 @@
 export const colors = {
-    robinsBlue: '#03c3cf',
-    pickledBluewood: '#2c3e50',
-    fountainBlue: '#67b1ca',
-    sherpaBlue: '#014e53',
-    bostonBlue: '#3F9DB7',
-    deepSeaGreen:'#09495F',
-    azureRadiance: '#117EEB'
-}
+  robinsBlue: '#03c3cf',
+  pickledBluewood: '#2c3e50',
+  fountainBlue: '#67b1ca',
+  sherpaBlue: '#014e53',
+  bostonBlue: '#3F9DB7',
+  deepSeaGreen: '#09495F',
+  azureRadiance: '#117EEB',
+  nobel: '#B6B6B6',
+};
--- app/view/mainScreen/index.js
+++ app/view/mainScreen/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {View, Text, ImageBackground, Button} from 'react-native';
-import { imgPath } from '../../modules/utils/images';
-import appLocalization from '../../localization/localization';
+import {ImageBackground, Button} from 'react-native';
+import {imgPath} from '../../modules/utils/images';
 import styles from './styles';
 
 const MainScreen = (props) => {
   return (
     <ImageBackground source={imgPath.mainBackground} style={styles.container}>
-      <Button onPress={()=> props.navigation.navigate('QuestionScreen')} title='Question Screen'/>
+      <Button
+        onPress={() => props.navigation.navigate('QuestionScreen')}
+        title="Question Screen"
+      />
+      <Button
+        onPress={() => props.navigation.navigate('PhotoScreen')}
+        title="Photo Screen"
+      />
     </ImageBackground>
   );
 };
