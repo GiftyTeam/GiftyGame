d19fa014232b6cb0a574b0c31b045638cf9c14d5
Aysel Heydarova <aysel.esedova1994@gmail.com>
2020-08-04 16:39:52 +0400

    phone-and-keyboardAvoid-issues-fixed

--- app/view/registrationScreen/styles.js
+++ app/view/registrationScreen/styles.js
@@ -1,9 +1,11 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+import {colors} from '../../modules/utils/colors';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 27,
+    justifyContent: 'flex-end',
   },
   logo: {
     width: 156,
@@ -12,6 +14,22 @@ const styles = StyleSheet.create({
     marginBottom: 90,
     marginTop: 130,
   },
+  input: {
+    height: 60,
+    backgroundColor: colors.mosque,
+    borderRadius: 18,
+    borderWidth: 5,
+    paddingHorizontal: 15,
+    borderColor: colors.fountainBlue,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'BalooChettan-Regular',
+    color: colors.white,
+    fontSize: 32,
+    height: 60,
+  },
+
+  inputText: {},
 });
 
 export default styles;
